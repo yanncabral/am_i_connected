c3b042276ba889f7ce52b7a7e71430dfba1ed318
Yann <iamyanndias@gmail.com>
2020-06-19 02:16:59 -0300

    initial release

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from . import am_i_connected
+
+__all__ = [
+    'check',
+]
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='am_i_connected',
+    version='0.1.2',
+    description='The simplest way to check if internet access is available.',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Yann Cabral',
+    author_email='iamyanndias@gmail.com',
+    keywords=['internet', 'check', 'connection'],
+    url='https://github.com/yanncabral/am_i_connected',
+    download_url='https://pypi.org/project/am_i_connected/'
+)
+
+install_requires = [
+    'socket'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
